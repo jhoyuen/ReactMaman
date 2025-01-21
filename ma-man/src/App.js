df6ba90e999ed4eabb8db02d3a0a928afa2524da
jhoyuen <jon_lg83@yahoo.com>
2025-01-21 21:20:37 +1100

    JRM-3: Dynamic values in Templates (#5)

--- ma-man/src/App.js
+++ ma-man/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const person = { name: 'yoshi', age: 41 }
+  const link = "http://www.google.com"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked <b>{likes} times</b></p>
+        <p>name: { person.name } / age: { person.age }</p>
+        <p>{ 10 }</p>
+        <p>{ "hello ninjas!" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>Die roll: { Math.ceil(Math.random() * 6) }</p>
+
+        <a href={ link }>Google Site</a>
       </div>
     </div>
   );
