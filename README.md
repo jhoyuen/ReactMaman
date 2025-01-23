c8bc123a582791064f977f2327ca1d827c5fefd3
jhoyuen <jon_lg83@yahoo.com>
2025-01-23 20:59:53 +1100

    JRM-4: Multiple Components

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ReactMaman
 My first React project Ma Man!
+- Install `Simple React Snippets` extension ()
+- Go to Settings > search for `Emmet` and choose `Emmet: Include Languages` > then add `"javascript": "javascriptreact"`
 
 # Lesson 1 - To create and run new React project
 `npx create-react-app ma-man`
@@ -11,3 +13,8 @@ NOTE: node_modules will need to be present, if not present, run `npm install`. I
 
 # Lesson 3 - Dynamic values in templates
 dynamic values within curly braces { } in jsx 
+
+# Lesson 4 - Multiple Components
+- Using `Simple React Snippets` extension:
+    - generate new stateless functional components (SFC) using `sfc` snippet shortcut
+    - generate new class template e.g `nav.navbar`
--- ma-man/src/App.js
+++ ma-man/src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const person = { name: 'yoshi', age: 41 }
-  const link = "http://www.google.com"
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked <b>{likes} times</b></p>
-        <p>name: { person.name } / age: { person.age }</p>
-        <p>{ 10 }</p>
-        <p>{ "hello ninjas!" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>Die roll: { Math.ceil(Math.random() * 6) }</p>
-
-        <a href={ link }>Google Site</a>
+        <Home />
       </div>
     </div>
   );
