2d904cc5f9d8895be1d37facb76a7541841994b6
jhoyuen <jonathan.klcflam@gmail.com>
2025-01-30 18:01:52 +1100

    JRM-6: handle click events

--- ma-man/src/Home.js
+++ ma-man/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {    
+        console.log('hello ma man!', e.target);
+    }
+
+    const handleClickAgain = (name, e) => {    
+        console.log('hello ' + name + ' ma man!', e.target);
+    }
     return (  
         <div className="home">
             <h2>homepage</h2>
+            <br/>
+            <button onClick={handleClick}>Click me</button>&nbsp;
+            <button onClick={(e) => handleClickAgain('Johan', e)}>Click me again</button>
         </div>
     );
 }
