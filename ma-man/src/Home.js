c8e382014aa34caaaf67f1fb04143a6311c4ff72
jhoyuen <jon_lg83@yahoo.com>
2025-02-06 21:52:51 +1100

    JRM-9: props (#11)

--- ma-man/src/Home.js
+++ ma-man/src/Home.js
@@ -1,20 +1,15 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs,setBlogs] = useState([
+    const [blogs] = useState([
         { id:1, title: 'My new website', body: 'lorem ipsum...', author: 'Mario' },
         { id:2, title: 'Welcome party!', body: 'lorem ipsum...', author: 'Luigi' },
         { id:3, title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Yoshi' }
     ]);
     return (  
         <div className="home">
-            { blogs.map((blog) => (
-                <div className="blog-preview" key={ blog.id }>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-                )) 
-            }    
+            <BlogList blogs={blogs} title="All Blogs" />
         </div>
     );
 }
