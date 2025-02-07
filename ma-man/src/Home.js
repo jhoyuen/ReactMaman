931abca3242b2e50ef496db5c778b26b3c0954cd
jhoyuen <jon_lg83@yahoo.com>
2025-02-07 18:08:12 +1100

    JRM-10: reuse components (#12)

--- ma-man/src/Home.js
+++ ma-man/src/Home.js
@@ -5,11 +5,12 @@ const Home = () => {
     const [blogs] = useState([
         { id:1, title: 'My new website', body: 'lorem ipsum...', author: 'Mario' },
         { id:2, title: 'Welcome party!', body: 'lorem ipsum...', author: 'Luigi' },
-        { id:3, title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Yoshi' }
+        { id:3, title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario' }
     ]);
     return (  
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs" />
         </div>
     );
 }
