fa59635017ad27498ca48d90129945f5c67e751f
jhoyuen <jon_lg83@yahoo.com>
2025-02-08 20:46:12 +1100

    JRM-11: functions as props (#13)

--- ma-man/src/Home.js
+++ ma-man/src/Home.js
@@ -2,15 +2,20 @@ import { useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs] = useState([
+    const [blogs, setBlogs] = useState([
         { id:1, title: 'My new website', body: 'lorem ipsum...', author: 'Mario' },
         { id:2, title: 'Welcome party!', body: 'lorem ipsum...', author: 'Luigi' },
         { id:3, title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario' }
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return (  
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs" />
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
         </div>
     );
 }
