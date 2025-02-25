59a179921e942862d8ec7150b13af78e5dd0d8d5
jhoyuen <jon_lg83@yahoo.com>
2025-02-26 10:02:30 +1100

    JRM-12: useEffect hook (#14)
    Co-authored-by: Zenyno <jonathan.klcflam@gmail.com>

--- ma-man/src/Home.js
+++ ma-man/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -12,6 +12,12 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    // runs every time the component renders
+    useEffect(() => {
+        console.log('useEffect ran');
+        console.log(blogs);
+    });
 
     return (  
         <div className="home">
