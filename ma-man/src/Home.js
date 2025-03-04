c29261fb7e3f404c0cb7fff9c834b1a8b89e4d69
jhoyuen <jon_lg83@yahoo.com>
2025-03-04 21:50:57 +1100

    JRM-16: conditional loading message (#18)
    Co-authored-by: Zenyno <jonathan.klcflam@gmail.com>

--- ma-man/src/Home.js
+++ ma-man/src/Home.js
@@ -3,21 +3,25 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     // runs every time the component renders by checking state changes for values in the dependencies array
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
-                console.log(data);
+                setIsPending(false);
             });
+        }, 1000);
     }, []);
 
     return (  
         <div className="home">
+            { isPending && <div>Loading...</div> }
             { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
         </div>
     );
