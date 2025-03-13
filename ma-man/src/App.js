2cc30097e15f8cfc9f62ea97c01810fb941da241
Zenyno <jonathan.klcflam@gmail.com>
2025-03-13 15:16:01 +1100

    JRM-19: The React Router

--- ma-man/src/App.js
+++ ma-man/src/App.js
@@ -1,15 +1,21 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </div>
+    </Router>
   );
 }
+
 
 export default App;
