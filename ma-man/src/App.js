3c748e330869a973935810142aef27f7becf8c56
jhoyuen <jon_lg83@yahoo.com>
2025-03-14 13:18:30 +1100

    JRM-23: add route parameters (#25)
    Co-authored-by: Zenyno <jonathan.klcflam@gmail.com>

--- ma-man/src/App.js
+++ ma-man/src/App.js
@@ -1,7 +1,8 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
         </Routes>
       </div>
     </div>
