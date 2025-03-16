53a911bb0b8ccdf6d7a1044dd3a5053e4ebd012f
jhoyuen <jon_lg83@yahoo.com>
2025-03-16 15:49:17 +1100

    JRM-29: delete blogs (#31)
    Co-authored-by: Zenyno <jonathan.klcflam@gmail.com>

--- ma-man/src/BlogDetails.js
+++ ma-man/src/BlogDetails.js
@@ -1,9 +1,20 @@
 import {useParams} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; 
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data:blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
+    const navigate = useNavigate();
+
+    const handleClick  = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            console.log('blog deleted');
+            navigate('/');
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -14,6 +25,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.content }</div>
+                    <button onClick={ handleClick }>Delete</button>  
                 </article>
             ) }
         </div>
